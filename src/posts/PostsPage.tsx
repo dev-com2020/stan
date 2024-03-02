4f7e4a0c5d890d0a98cd2166b31ceb514f6e6219
DEV-COM <kwp@tlen.pl>
2024-03-02 14:34:13 +0100

    14:34 2.3

--- src/posts/PostsPage.tsx
+++ src/posts/PostsPage.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
-import { PostData } from "./types";
+import { NewPostData, PostData } from "./types";
 import { getPosts } from "./getPosts";
 import { PostsList } from "./PostsList";
+import { NewPostForm } from "./NewPostForm";
+import { savePost } from "./savePost";
 
 export function PostsPage() {
     const [isLoading, setIsLoading] = useState(true)
@@ -19,12 +21,17 @@ export function PostsPage() {
             cancel = true
         }
     }, [])
+    async function handleSave(NewPostData: NewPostData){
+        const newPost = await savePost(NewPostData)
+        setPosts([newPost,...posts])
+    }
     if (isLoading) {
         return <div>Ładuje...</div>
     }
 
     return (
         <div>
+            <NewPostForm onSave={handleSave}/>
             <h2>Posty:</h2>
             <PostsList posts={posts} />
         </div>
