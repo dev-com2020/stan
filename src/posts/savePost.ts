e9b2a1d24a57137b0fa398bee4b91304d68e1259
DEV-COM <kwp@tlen.pl>
2024-03-02 14:05:48 +0100

    14:05 2.3

---
+++ src/posts/savePost.ts
@@ -0,0 +1,22 @@
+import { NewPostData, SavedPostData } from "./types";
+
+export async function savePost(newPostData: NewPostData) {
+    const response = await fetch(
+        process.env.REACT_APP_API_URL!,{
+            method: 'POST',
+            body: JSON.stringify(newPostData),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }
+    )
+    const body = (await response.json()) as unknown
+    assertIsSavedPost(body)
+    return {...newPostData, ...body}
+}
+function assertIsSavedPost(post: any): asserts post is SavedPostData {
+    if (!('id' in post)) {
+        throw new Error("pod nie zawiera id")
+    }
+
+}
