dcf93d639e4dad9aef97525cbb026b9dcd0d7fe2
DEV-COM <kwp@tlen.pl>
2024-03-02 15:43:37 +0100

    15:43 2.3

---
+++ src/repoPage/StarRepoButton.tsx
@@ -0,0 +1,10 @@
+type Props = {
+    onClick: () => void
+}
+
+export function StarRepoButton({onClick}: Props){
+    return (
+        <button type="button"
+        onClick={onClick}>Star</button>
+    )
+}
