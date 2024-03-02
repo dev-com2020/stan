49b9d052095a48d264dfe4ea0c722d7ebd7f9b44
DEV-COM <kwp@tlen.pl>
2024-03-02 09:31:32 +0100

    09:31 2.3

--- src/Header.tsx
+++ src/Header.tsx
@@ -5,3 +5,19 @@ type Props = {
     onSignInClick: () => void
     loading: boolean
 }
+
+export function Header({
+    user,
+    onSignInClick,
+    loading,
+}: Props) {
+    <header>
+        { user ? (<span>
+            {user.name} has signed in
+        </span>) : (
+            <button onClick={onSignInClick} disabled={loading}>
+                {loading ? '...' :'Zaloguj się'}
+            </button>
+        )}
+    </header>
+}
