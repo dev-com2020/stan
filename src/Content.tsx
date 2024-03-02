6fc971b7df660a7cb5fe36bb9fbb516eace0cb6a
DEV-COM <kwp@tlen.pl>
2024-03-02 12:37:08 +0100

    poprawiony redux 2.3

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,21 +1,16 @@
-import { useSelector } from "react-redux"
-import { RootState } from "./store/store"
+import { useSelector } from 'react-redux';
+import { RootState } from './store/store';
 
+export function Content() {
+  const permissions = useSelector((state: RootState) => state.user.permissions);
 
-export function Content(){
-    const permissions = useSelector(
-        (state: RootState) => state.user.permissions)
-        
-    if (permissions === undefined){
-        return null
-    }
-    return permissions.includes('admin') ? (
-        <p>
-            Ważne rzeczy robi tylko admin
-        </p>
-    ) : (
-        <p>
-            Zbyt małe uprawnienia
-        </p>
-    )
+  if (permissions === undefined) {
+    return null;
+  }
+
+  return permissions.includes('admin') ? (
+    <p>Ważne rzeczy robi admin</p>
+  ) : (
+    <p>Nie masz uprawnień</p>
+  );
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,18 +1,23 @@
-import { useDispatch, useSelector } from 'react-redux';
 import { authenticate } from './api/authenticate';
 import { authorize } from './api/authorize';
-import { RootState } from './store/store';
-import { authenticateAction, authenticatedAction, authorizeAction, authorizedAction } from './store/userSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import type { RootState } from './store/store';
+import {
+  authenticateAction,
+  authenticatedAction,
+  authorizeAction,
+  authorizedAction,
+} from './store/userSlice';
 
 export function Header() {
-  const user = useSelector((state: RootState) => state.user.user)
-  const loading = useSelector((state: RootState) => state.user.loading)
-  const dispatch = useDispatch()
+  const user = useSelector((state: RootState) => state.user.user);
+  const loading = useSelector((state: RootState) => state.user.loading);
+  const dispatch = useDispatch();
 
   async function handleSignInClick() {
     dispatch(authenticateAction());
     const authenticatedUser = await authenticate();
-    dispatch(authenticatedAction());
+    dispatch(authenticatedAction(authenticatedUser));
     if (authenticatedUser !== undefined) {
       dispatch(authorizeAction());
       const authorizedPermissions = await authorize(authenticatedUser.id);
@@ -20,16 +25,15 @@ export function Header() {
     }
   }
   return (
-    <header className="flex justify-between items-center border-b-2 border-gray-100 py-6">
+    <header>
       {user ? (
-        <span className="ml-auto font-bold">{user.name} has signed in</span>
+        <span>{user.name} has signed in</span>
       ) : (
         <button
           onClick={handleSignInClick}
-          className="whitespace-nowrap inline-flex items-center justify-center ml-auto px-4 py-2 w-36 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
           disabled={loading}
         >
-          {loading ? '...' : 'Sign in'}
+          {loading ? '...' : 'Zaloguj się'}
         </button>
       )}
     </header>
