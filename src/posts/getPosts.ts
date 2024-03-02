f01a711d05f0e3370c8e8e9a42630d7c1b704bc3
DEV-COM <kwp@tlen.pl>
2024-03-02 12:31:01 +0100

    12:30 2.3

--- src/posts/getPosts.ts
+++ src/posts/getPosts.ts
@@ -1,9 +1,30 @@
+import { PostData } from "./types"
 
 
 export async function getPosts(){
     const response = await fetch(
         process.env.REACT_APP_API_URL!
     )
-    const body = await response.json()
+    const body = (await response.json()) as unknown
+    assertIsPosts(body)
     return body
 }
+
+export function assertIsPosts(
+    postsData: unknown): 
+    asserts postsData is PostData[]{
+        if (!Array.isArray(postsData)){
+            throw new Error("to nie jest tablica")
+        }
+        if (postsData.length === 0) {
+            return
+        }
+        postsData.forEach((post) => {
+            if (!('id' in post)) {
+                throw new Error("brak ID")
+            }
+            if (typeof post.id !== 'number'){
+                throw new Error("id nie jest numerem")
+            }
+        })
+    }
