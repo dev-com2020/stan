e174e50014aa7cd343ca3d8d983857f90bd8504c
DEV-COM <kwp@tlen.pl>
2024-03-02 11:30:05 +0100

    redux 2.3

---
+++ src/store/store.ts
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { useReducer } from "react";
+
+export const store = configureStore({
+    reducer: {user: useReducer}
+})
+
+export type RootState = ReturnType<typeof store.getState>
