0b919cb6b891119263360294380e6d192f269022
DEV-COM <kwp@tlen.pl>
2024-03-03 11:37:49 +0100

    testy 11:37 3.3

--- src/Checklist/Checklist.test.tsx
+++ src/Checklist/Checklist.test.tsx
@@ -1,14 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import { CheckList } from './Checklist';
+import exp from 'constants';
 
 test('renders CheckList with data', () => {
     render(
         <CheckList 
-        data={[{ id: '1', primary: 'Item 1', secondary: 'Description 1'}]}   
+        data={[{ id: '1', primary: 'Item 1', secondary: 'Manager'}]}   
         id = 'id'
         primary = 'primary'
         secondary = 'secondary'
         />
     )
     expect(screen.getByText('Item 1')).toBeInTheDocument()
+    expect(screen.getByText('Manager')).toBeInTheDocument()
+});
+
+test('should render correct list item', () => {
+    render(
+        <CheckList 
+        data={[{ id: '1', primary: 'Item 1', secondary: 'Manager'}]}   
+        id = 'id'
+        primary = 'primary'
+        secondary = 'secondary'
+        renderItem={(item) => (
+            <li key={item.id}>
+                {item.primary} -- {item.secondary}
+            </li>
+        )}
+        />
+    )
+    expect(screen.getByText('Item 1 -- Manager')).toBeInTheDocument()
 });
